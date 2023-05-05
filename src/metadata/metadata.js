c2a7ec05a272d54129e7b0cb44d775aac1385255
Dimitri Gerin <dimitri.gerin@gmail.com>
2023-05-05 20:03:24 +0200

    my cv

--- src/metadata/metadata.js
+++ src/metadata/metadata.js
@@ -1,54 +1,170 @@
 module.exports = {
-  name: 'William Shakespeare',
-  title: 'Poet, playwright and actor',
+  name: 'Dimitri Gerin',
+  title: 'Software and Application Engineer',
   facts: {
-    'Residence': '<a href="https://www.google.com/maps/place/London,+UK/@51.5285582,-0.2416814,11z/data=!3m1!4b1!4m5!3m4!1s0x47d8a00baf21de75:0x52963a5addd52a99!8m2!3d51.5072178!4d-0.1275862"><i class="fa fa-home fact-icon"></i>London</a>',
-    'Wiki': `<a href="https://en.wikipedia.org/wiki/William_Shakespeare">
-                  <i class="fa-brands fa-wikipedia-w fact-icon"></i>William_Shakespeare
-                </a>`,
-    'Twitter': `<a href="https://twitter.com/shakespeare"><i class="fa-brands fa-twitter fact-icon"></i>Shakespeare</a>`,
-    'Email': '<a href="mailto:wil64@yahoo.com"><i class="fa fa-envelope fact-icon"></i>wil64@yahoo.com</a>'
+    'residence': '<a href="https://goo.gl/maps/H94JiPQpQ76JBTpq9"><i class="fa fa-home fact-icon"></i>2B Rue du 11 Novembre 1918, Palaiseau</a>',
+    //'Wiki': `<a href="https://en.wikipedia.org/wiki/William_Shakespeare">
+    //              <i class="fa-brands fa-wikipedia-w fact-icon"></i>William_Shakespeare
+    //            </a>`,
+    //'Twitter': `<a href="https://twitter.com/shakespeare"><i class="fa-brands fa-twitter fact-icon"></i>Shakespeare</a>`,
+    'email': '<a href="dgerin@poton.me"><i class="fa fa-envelope fact-icon"></i>dgerin@poton.me</a>',
+    'phone': '<i class="fas fa-phone fact-icon"></i>+33783835501</a>',
+    'linkedin': '<a href="https://www.linkedin.com/in/dimitri-gerin-04026960"><i class="fab fa-linkedin fact-icon"></i>Dimitri Gerin</a>',
+    'github pro (UPMEM)': '<a href="https://github.com/dgerinmem"><i class="fab fa-github fact-icon"></i>(UPMEM Pro github) dgerinmem</a>',
+    'github perso (UPMEM)': '<a href="https://github.com/dgerin"><i class="fab fa-github fact-icon"></i>(Personal github) dgerin</a>'
   },
   skills: [
-    ['Comedy', 100],
-    ['Love', 100],
-    ['Theater', 100],
-    ['Tragedy', 100],
-    ['Poetry', 100],
-    ['Erotic', 75],
-    ['Sonnets', 100],
-    ['Plays', 100],
-    ['Writing', 100],
-    ['Directing', 75],
-    ['Literature', 100],
-    ['Influence', 100],
+    ['parrallel programming', 100],
+    ['performances optimization', 90],
+    ['Linux device driver', 60],
+    ['Deep Learning', 50],
+    ['RTL Design', 80],
+    ['Quantization', 70],
+  ],
+  languages: [
+    ['C', 100],
+    ['C++', 100],
+    ['Python', 100],
+    ['VHDL', 100],
+  ],
+  systems: [
+    ['PIM (Processing in Memory)', 100],
+    ['Linux Kernel', 60],
+    ['x86', 60],
+    ['PCI express', 60],
+    ['DDR Memory', 100],
+    ['Xilinx FPGA', 100],
+  ],
+  frameworks: [
+    ['PyTorch', 100],
+    ['Tensorflow', 70],
+    ['LLVM', 40],
+  ],
+  tools: [
+    ['linux perf', 100],
+    ['qemu', 70],
+    ['intel vtune', 60],
+    ['Docker', 60],
+    ['vivado', 60],
   ],
   positions: [
     {
-      title: 'Hamlet',
-      period: 'Likely early 17th century',
-      skills: ['tragedy', 'philosophy', 'death'],
-      contents: `
-Prince Hamlet is visited by his father's ghost and ordered to avenge his father's murder by killing King Claudius, his uncle. After struggling with several questions, including whether what the ghost said is true and whether it is right for him to take revenge, Hamlet, along with almost all the other major characters, is killed.
-`
+      title: 'UPMEM',
+      location: 'Paris/Grenoble',
+      position: 'Software and Application Engineer',
+      period: '2021 - 2023',
+      skills: ['PIM', 'Linux Device Driver', 'Parallel Programming', 'PCI express' ,'Performance Optimization', 'Deep Learning'],
+      contents: 'One part of my job is to develop applications on UPMEM PIM devices to demonstrate the interest of PIM technology in term of performance and energy efficiency. I also work on the development, maintenance and support of the UPMEM PIM SDK, which is a set of tools and libraries to develop applications on UPMEM PIM devices. \
+                I am also working on the improvement of the UPMEM PIM device driver, which is a Linux kernel module used to communicate with PIM devices.'
+                ,
+      project_section : 'Projects and applications',
+      projects :[
+          { name : 'genomic compression', content : 'Implementation of a a novel genomique technique of DNA comprehension, based on blool filter, in colaboration with INRIA/IRISA.' },
+          { name : 'SparseP', ref :'https://github.com/upmem/SparseP/tree/upmem_internal', content : 'Obtimization of original SparseP implementation from ETH Zurich. SparseP is a PIM implementation of Sparse Matrix/Vector multiplication (https://arxiv.org/abs/2201.05072)' },
+          { name : 'PIM Embedding', ref : 'https://github.com/upmem/PIM-Embedding-Lookup/tree/multicol' , content : 'PIM implementation of Pytorch EmbeddingBag ' },
+      ],
     },
     {
-      title: 'Twelfth Night',
-      period: '1600 – 1601',
-      skills: ['comedy', 'love'],
-      contents: `
-Viola finds herself shipwrecked in Illyria and, assuming that her brother Sebastian has died in the wreck, disguises herself as a man to gain a position in Duke Orsino's court. Orsino sends Viola (whom he knows as Cesario) to deliver a message to his love, Olivia. Olivia, however, dislikes the Duke. She falls in love with Viola, who she thinks is a man. Eventually, Viola's brother Sebastian, who in fact was unharmed in the wreck, reappears. At a critical moment, Viola's true identity is revealed when members of the court notice the similarities between her and Sebastian. Olivia quickly falls in love with Sebastian, and Viola confesses her love for the Duke.
-`
+      title: 'VSORA',
+      location: 'Meudon-La-Forêt',
+      position: 'Software Engineer',
+      period: '2018 - 2021',
+      skills: ['DeepLearning', 'C++', 'SystemC'],
+      contents: 'I mainly focussed on the specification and the implementation of the VSORA Deep Learning Inference\
+      library, a library that implements various DL Layers on VSORA DSP Architecture.\
+      I also ported various Deep Learning models on VSORA DSP Architecture, and developed a Quantization Aware retraining flow to improve\
+      models precision with Low-Precision quantization.\
+      One other part of my job was to maintain the release and CI pipeline.'
     },
     {
-      title: 'The Tragedy of Romeo and Juliet',
-      period: '1595–1596, with a possible early draft written in 1591',
-      skills: ['tragedy', 'death', 'love'],
-      contents: `
-In Verona, Italy, two families, the Montagues and the Capulets, are in the midst of a bloody feud. Romeo, a Montague, and Juliet, a Capulet, fall in love and struggle to maintain their relationship in the face of familial hatred. After Romeo kills Juliet's cousin Tybalt in a fit of passion, things fall apart. Both lovers eventually commit suicide within minutes of each other, and the feuding families make peace over their recent grief.
-`
+      title: 'THALES',
+      location: 'Gennevilliers',
+      position: 'FPGA Engineer',
+      period: '2017 - 2018',
+      skills: ['FPGA', 'Telecom', 'Signal Processing'],
+      contents: `As subcontractor, I participaped to the development and validation of the physical layer of FO3D, a new military waveform developed by Thales.
+       I focussed myself on the design of the FPGA part of the TX and RX chains. For this, I developed
+       different RTL blocks that implements signal processing functions.`,
+      project_section : 'Projects',
+      projects :[
+          { name : 'FO3D', content : 'FPGA Implementation of the physical layer of the FO3D waveform' },
+      ],
+    },
+    {
+      title: 'SIMPULSE',
+      location: 'Palaiseau',
+      position: 'FPGA / Embedded System Engineer',
+      period: '2014 - 2017',
+      skills: ['RTL Design', 'FPGA', 'Telecom','SoC', 'HW/SW Codesign'],
+      contents: `I participated to the development of the FPGA platform. I developed different RTL blocks for
+                 different purposes like telecom signal processing functions, device interfaces and CPU co-processors.`,
+      project_section : 'Noticable Projects',
+      projects :[
+          { name : 'European Project', content : 'In colaboration with Nokia Bell Labs, implementing an OpenAIR LTE If4p5 Interface, into FPGA, with Simpulse DSP IP' },
+      ],
+    },
+    {
+      title: 'CEA LETI',
+      location: 'Grenoble',
+      period: '2014',
+      position: 'Intern',
+      skills: ['Matlab', 'OFDM', '5G'],
+      contents: `Research in MIMO algorithm for advanced (5G,LTE-Advance).`
+    },
+    {
+      title: 'UNLV',
+      location: 'Las Vegas',
+      position: 'Intern',
+      period: '2013',
+      skills: ['C++'],
+      contents: `Participating to developpment of an interactive motion based traffic simulator.`
     }
   ],
+  education :[
+    {
+      period: '2012 - 2014',
+      header: 'PHELMA, Grenoble-INP (ex ENSERG)',
+      location: 'Grenoble',
+      skills: ['Signal Processing','Electrical Engineering','Computer Science'],
+      contents: `Engineer's Degree in signal processing, electrical engineering and computer science`
+    },
+    {
+      period: '2008 - 2010',
+      header: 'CNAM',
+      location: 'Paris',
+      skills: ['Electrical Engineering'],
+      contents: `Technology degree (DUT GEII), Graduated as Valedictorian`
+    },
+    {
+      period: '2005 - 2008',
+      header: 'ETPLM',
+      location: 'Grenoble',
+      contents: `High school degree (BAC STI GE), with hounours (mention très bien)`
+    },
+  ],
+   certifications :[
+    {
+      period: '2021',
+      header: 'Machine Learning',
+      provider: 'Coursera/Standford, Andrew Ng',
+      skills: ['Logistic Regression', 'Neural Networks', 'Recommenders'],
+      contents: ``
+    },
+    {
+      period: '2022',
+      header: 'Solana Blockchain Developer Bootcamp with Rust + JavaScript',
+      provider: 'Udemy',
+      skills: ['SmartContract', 'Rust', 'Javascript', 'Solana'],
+      contents: ``
+    },
+    {
+      period: '2018',
+      header: 'Xilinix FPGA Certification',
+      provider: 'Xilinx Training Program',
+      skills: ['FPGA', 'Vivado', 'VHDL'],
+      contents: ``
+    }
+   ],
   experience: [
     {
       header: 'Performance',
